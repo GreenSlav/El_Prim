fd99efad572d7760484eb36a394298780e428ed9
GreenSlav <sromanov2608@gmail.com>
2024-04-22 01:38:43 +0300

    stable, adaptive screen

--- GraphsMT/Graphs/App.xaml.cs
+++ GraphsMT/Graphs/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace Graphs;
+﻿//using System.Windows.Forms;
+
+namespace Graphs;
 
 public partial class App : Application
 {
@@ -7,5 +9,27 @@ public partial class App : Application
         InitializeComponent();
 
         MainPage = new AppShell();
+    }
+    
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        var window = base.CreateWindow(activationState);
+
+        // разрешение окна
+        // const int newWidth = 1920;
+        // const int newHeight = 1080;
+        // перед этим нужно не забыть добавить reference к C:\WINDOWS\Microsoft.NET\Framework\v4...\System.Windows.Forms.dll
+        // и скачать через нагетсы этот пакет исключений: Microsoft.Win32.SystemEvents
+        int newWidth = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+        int newHeight = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+        
+        // где должно появляться окно
+        window.X = 0;
+        window.Y = 0;
+        
+        window.Width = newWidth;
+        window.Height = newHeight;
+
+        return window;
     }
 }
---
+++ GraphsMT/Graphs/README.md
@@ -0,0 +1,3 @@
+﻿### Dependencies и все такое:
+- Указать reference проекта с приложением к System.Windows.Forms.dll 
+- Скачать nugets к проекту: Microsoft.Win32.SystemEvents
