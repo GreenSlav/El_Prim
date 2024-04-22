172a3d29f884877e35b064bf74e534369a92e166
GreenSlav <sromanov2608@gmail.com>
2024-04-22 03:14:52 +0300

    Stable, changed font in title view

--- GraphsMT/Graphs/MauiProgram.cs
+++ GraphsMT/Graphs/MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-             
+                fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
+                fonts.AddFont("Caveat-VariableFont_wght.ttf", "Caveat");
             });
 
 #if DEBUG
